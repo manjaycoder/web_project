dca4b2f966b87d24fde573688396b7ab9d2c5dda
Manjay Verma <161963231+manjaycoder@users.noreply.github.com>
2024-08-17 17:03:31 +0530

    page1 done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var t1=gsap.timeline()
+
+function loader(){
+    var t1=gsap.timeline()
 t1.from(".line h1",{
     y:150,
     stagger:0.25,
@@ -45,12 +47,36 @@ t1.from("#page1",{
     delay:0.2,
     y:1600,
     opacity:0,
-    ease:power4,
+    ease:"power4",
     duraton:0.5,
 
 })
-t1.to("#loader",{
-    display:NamedNodeMap,
+t1.from("#nav",{
+    opacity:0
 })
+t1.from("#hero1 h1,#hero2 h1,#hero3 h2 ,#hero4 h1",{
+    y:120,
+    stagger:0.2,
+})
+
+
+
+}
+function cursoranimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#cur",{
+            x:dets.x,
+            y:dets.y,
+        })
+    })
+    Shery.makeMagnet("#nav_part2 h4", {});
+}
+loader()
+cursoranimation()
+
+
+ 
+
+
 
 
